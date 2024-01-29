be2b9d53bf17de2f237bbc984804f52974bf8d00
harwedah <alabiaweda619@gmail.com>
2024-01-29 14:54:38 -0800

    netflix

---
+++ netflix.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+const head = document.querySelector('.header');
+const headContent = document.querySelector('header-content');
+const upLanguageBtn = document.querySelector('language-btn');
+const upSigninBtn = document.querySelector('signin-btn');
+const emailSignup = document.querySelector('.email-signup')
+const emailInput = document.querySelector('#mail')
+const getStartedBtn = document.querySelector('.get-started')
+const features = document.querySelector('features');
+
+
+//Now assignimg begins here
+//IDK 
+//what do do yet at 20/01/24
+
+})
